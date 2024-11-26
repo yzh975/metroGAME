69857df1f88d87fd20f7900d213899566b6a2c32
faye <1757509119@qq.com>
2024-11-26 20:39:13 +0800

    1

--- src/model/Clock.java
+++ src/model/Clock.java
@@ -1,5 +1,26 @@
 package model;
 
 public class Clock {
+    private static final String[] weekDay= {"MON", "TUE", "WED", "THU", "FRI", "SAT", "SUN"};
+    int currentDay;
+    int time;
 
+    public int getTime() {
+        return time;
+    }
+
+    public void getDay(){
+        return weekDay[currentDay];
+    }
+
+    public void incrementTime(){
+        time++;
+        //to be continued..
+    }
+
+    public void incrementDay(){
+        currentDay++;
+        if(currentDay>=7)
+            currentDay=0;
+    }
 }
